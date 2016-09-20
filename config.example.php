a2e8b15bcab05938085379f18d6031412c1d0dc8
ulikoenig <github@bills-erben.de>
2016-09-20 15:43:27 +0200

    Example for config

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "mysql.server.net";
+    $user = "pewosa";
+    $pass = "123456789";
+    $dbname = "pewosa";
+    date_default_timezone_set('Europe/Berlin');
+    setlocale(LC_TIME, "de_DE");
+    $defaultPressesprecherID = 2; 
+?>
