88aebc61e69860058ea33d0f165f6f981d55fd68
ulikoenig <github@bills-erben.de>
2016-10-03 17:09:32 +0200

    kleinkram

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@ require("UrlLinker.php");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-header("Content-Type: text/plain"); 
+header("Content-Type: text/plain; charset=utf-8"); 
 
 $verbose=TRUE;
 $time_start = microtime(true);
