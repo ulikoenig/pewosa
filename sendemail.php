5a724cd8db8c0ee0b722fc4b79fe14ff3fff9628
christianlewin <christian.lewin@gmx.net>
2017-02-10 10:32:25 +0100

    Fehler behoben
    Mehrfache Mails sollten seltener oder gar nicht mehr auftreten. Nur ein Prozessaufruf erlaubt.

--- sendemail.php
+++ sendemail.php
@@ -8,6 +8,8 @@ header ( "Cache-Control: post-check=0, pre-check=0", false );
 header ( "Pragma: no-cache" );
 header ( "Content-Type: text/plain; charset=utf-8" );
 
+define("VERBOSE", "TRUE");
+
 $verbose = TRUE;
 $time_start = microtime ( true );
 function bcrypt_encode($email, $password, $rounds = '08') {
@@ -15,7 +17,7 @@ function bcrypt_encode($email, $password, $rounds = '08') {
 	$salt = substr ( str_shuffle ( './0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' ), 0, 22 );
 	return crypt ( $string, '$2a$' . $rounds . '$' . $salt );
 }
-function sendpm($subject, $body, $receiverMail, $receiverName, $attachment, $id) {
+function sendpm($subject, $body, $receiverMail, $receiverName, $attachment, $id, $hash) {
 	$mail = new PHPMailer ();
 	
 	// $mail->SMTPDebug = 3; // Enable verbose debug output
@@ -48,7 +50,8 @@ function sendpm($subject, $body, $receiverMail, $receiverName, $attachment, $id)
 	
 	// Wir verschlüsseln id und email, um uns gegen Missbrauch abzusichern
 	If ($receiverMail != 'presse@piratenfraktion-sh.de' and $receiverMail != 'presseticker@ltsh.de') {
-		$hash = md5 ( $receiverMail . $id );
+		//$hash = md5 ( $receiverMail . $customerid );
+		//hash bleibt hash
 	} else {
 		// Sollen keinen echten Abmeldelink enthalten
 		$hash = 'nocando';
@@ -104,21 +107,87 @@ function runtime() {
 	return microtime ( true ) - $_SERVER ["REQUEST_TIME_FLOAT"];
 }
 
+
+
+If ($verbose) {
+	echo "Versand der E-Mails vorbereiten...\n";
+}
+$stillsending = true;
+
+define("LOCKFILENAME", "send.lock");
+
+function updateLock($lockTime){
+	$handle = fopen (LOCKFILENAME, "w");
+	$clockout=date("Y-m-d H:i:s", time() + $lockTime);
+	fwrite ($handle, $clockout);
+	fclose ($handle);
+}
+/**
+* Verhindert, dass mehrere Instanzen gleichzeitig arbeiten. MUTEX
+*/
+function getLock($lockTime){
+	//Wir wollen verhindern, dass Parallelprozesse starten
+	
+	//Wird gearbeitet wird eine einzelne Instanz gestartet / läuft bereits eine, wird der Prozess abgebrochen
+	$handle = fopen (LOCKFILENAME, "rb");
+	If (filesize (LOCKFILENAME)>0)
+		{
+		$inhalt = fread ($handle, filesize (LOCKFILENAME));
+		}
+	else
+		{
+		$inhalt='';	
+		}
+	fclose ($handle);
+
+	If ($inhalt!='')
+		{
+		If (VERBOSE) 
+			{
+			echo "Prozess läuft maximal bis $inhalt\n";
+			}			
+		//Hier testen wir auf toten Prozess
+		$date1 = date ( "Y-m-d H:i:s" );
+		$dateTimestampaktuell = strtotime ( $date1 );
+		$dateTimestampdatei = strtotime ( $inhalt );	
+		If ($dateTimestampaktuell > $dateTimestampdatei) 
+			{
+			//Instanz ist abgelaufen und wird löschen
+			$handle = fopen(LOCKFILENAME, "w"); 
+			fclose($handle);	
+			If (VERBOSE) 
+				{
+				echo "Prozess wurde offensichtlich ungeplant beendet -  Prozess zurückgesetzt\n";
+				}			
+			}
+		else
+			{
+			//Hier gehts normal weiter
+			echo "Fehler: Es läuft bereits ein Prozess -> keine weitere Instanz zugelassen\n";
+			exit ( - 1 );
+			}
+		}
+		echo "Neuer Prozess gestartet\n";	
+		updateLock($lockTime);
+}
+
 /**
  * PM Senden und sicherstellen, dass keine E-Mails doppelt raus gehen
  */
-function sendPmAfterCheck($counter, $sendstate, $email, $all_mails, $pmid, $subject, $body, $email, $name, $pdf, $verbose, $distribution_id) {
+function sendPmAfterCheck($counter, $sendstate, $email, &$all_mails, $pmid, $subject, $body, $name, $pdf, $verbose, $distribution_id, $hash) {
 	// Bereits gesendete Mails nicht noch mal senden
+	//$sendstate = -1;
 	if ($counter > $sendstate) {
 		// Nur schicken wenn Mailadresse unique
 		If (! in_array ( $email, $all_mails )) {
 			// echo "sendpm(PM: $subject,$body,$email,$name,PDF);";
 			setpmsending ( $pmid, $counter );
-			sendpm ( $subject, $body, $email, $name, $pdf, $pmid );
+			sendpm ( $subject, $body, $email, $name, $pdf, $pmid, $hash );
 			// Wir müssen alle benutzten Mails speichern, um zu vermeiden, dass wir an eine Mailadresse doppelt rausschicken
 			array_push ( $all_mails, $email );
 			If ($verbose) {
-				echo "PM $pmid wurde an $email aus dem Verteiler $distribution_id verschickt...\n";
+				echo "PM $pmid wurde an $email aus dem Verteiler/Empfänger $distribution_id verschickt...\n";
+				//print_r ($all_mails);
 			}
 		}
 	} else {
@@ -126,24 +195,16 @@ function sendPmAfterCheck($counter, $sendstate, $email, $all_mails, $pmid, $subj
 			echo "Resume: Überspringe $email in PM $pmid: Counter = $counter; Sendstate = $sendstate\n";
 		}
 	}
+	updateLock(10);
 }
 
-If ($verbose) {
-	echo "Versand der E-Mails vorbereiten...\n";
-}
-$stillsending = true;
 
-// / Mutex Init
+getLock(10);
 
 while ( $stillsending ) {
-	// Warte auf Mutex
-	$fp = fopen ( './send.lock', 'r+' ) or die ( $php_errormsg );
-	if (! flock ( $fp, LOCK_EX )) {
-		echo 'ERROR: Unable to obtain lock. --> Terminating';
-		fclose ( $fp );
-		exit ( - 1 );
-	}
+	
 	$counter = 0;
+	$counterPMFound = 0;
 	$pmid = 0;
 	$sendstate = 0;
 	$query = "SELECT `pressrelease`.*, `users`.firstname, `users`.lastname, `users`.jobtitle, `users`.phone FROM `pressrelease`, `users` WHERE `pressrelease`.pressagentid = `users`.id AND senddate IS NOT NULL AND sendnow <> 2 AND senddate >0 AND ((sendstate = -1) OR (sendstate > 0)) AND senddate < CONVERT_TZ( NOW( ) , '" . SQLSERVERTIMEZONE . "', '" . SQLLOCALTIMEZONE . "' )  Order by id ASC LIMIT 1";
@@ -160,7 +221,7 @@ while ( $stillsending ) {
 				echo "Resume: Überspringe die ersten $sendstate Empfänger\n";
 			}
 			If ($dateTimestamp2 <= $dateTimestamp1) {
-				$counter ++;
+				$counterPMFound ++;
 				echo "RowID " . $row->id . "\n";
 				$pmid = $row->id;
 				$subject = $row->subject;
@@ -169,20 +230,41 @@ while ( $stillsending ) {
 			}
 		}
 	} else
+	{
 		$stillsending = false; // KEINE PMs mehr gefunden
+		$pmid = 0;
+		$counter = 0;
+		$counterPMFound = 0;
+		$sendstate = 0;
+		unset ($date1);
+		unset ($date2);
+		unset ($dateTimestamp1);
+		unset ($dateTimestamp2);
+		unset ($subject);
+		unset ($body);
+		unset ($pdf);
+		//Instanz freigeben
+		$handle = fopen(LOCKFILENAME, "w"); 
+		fclose($handle);	
+		If ($verbose) 
+			{
+			echo "Prozess wurde beendet...\n";
+			}		
+		}
 	
 	If ($verbose) {
-		echo "Habe $counter zu versendende PMs gefunden...\n";
+		echo "Habe $counterPMFound zu versendende PMs gefunden...\n";
 	}
 	
 	// Initialisieren von dem Array in dem wir alle angeschriebenen Mailadressen speichern
+	unset ($all_mails);
 	$all_mails [0] = "no@real.mail";
 	
 	if ($pmid > 0) {
 		// Checken: soll es an Verteilerlisten raus, soll es an Einzelempfänger raus
 		$send_distribution = FALSE;
 		$send_customer = FALSE;
-		$query = "SELECT listID,customerID FROM pressreleaseconnection WHERE pressreleaseID = $pmid";
+		$query = "SELECT listID,customerID FROM pressreleaseconnection WHERE pressreleaseID = $pmid AND (listID IS NOT NULL OR customerID IS NOT NULL)";
 		$checkdata = mysql_query ( $query );
 		if (mysql_num_rows ( $checkdata ) >= 1) {
 			while ( $row = mysql_fetch_object ( $checkdata ) ) {
@@ -196,9 +278,9 @@ while ( $stillsending ) {
 		}
 		// Erstmal an Verteilerlisten schicken
 		If ($send_distribution) {
-			// Eingefügt AND `customer`.`deleted`!='1', damit nur aktive angeschrieben werden
+			// Eingefügt AND `customer`.`deleted`!='1', damit nur aktive angeschrieben werden + !=2 weil wir ja nun unterscheiden unter selbstabgemeldet und von uns deaktiviert
 			// Hoffe das klappt
-			$query = "SELECT  `customer`.id,`customer`.email, `customer`.firstname,  `customer`.lastname,`pressreleaseconnection`.listID FROM `pressrelease`, `pressreleaseconnection`, `customerdistribution`, `customer` WHERE `pressrelease`.`id` = $pmid and `pressreleaseconnection`.pressreleaseID = $pmid  AND `customerdistribution`.distribution = `pressreleaseconnection`.listID AND `customer`.id = `customerdistribution`.customer  AND `customer`.`deleted`!='1' AND `pressreleaseconnection`.`listID` IS NOT NULL  GROUP BY `customerdistribution`.customer";
+			$query = "SELECT  `customer`.id,`customer`.hash,`customer`.email, `customer`.firstname, `customer`.lastname,`pressreleaseconnection`.listID FROM `pressrelease`, `pressreleaseconnection`, `customerdistribution`, `customer` WHERE `pressrelease`.`id` = $pmid and `pressreleaseconnection`.pressreleaseID = $pmid  AND `customerdistribution`.distribution = `pressreleaseconnection`.listID AND `customer`.id = `customerdistribution`.customer  AND `customer`.`deleted`!='1' AND `customer`.`deleted`!='2' AND `pressreleaseconnection`.`listID` IS NOT NULL GROUP BY `customer`.id ORDER BY `customer`.id";
 			// echo "Abfrage 1: $query";
 			$checkdata = mysql_query ( $query )  or die ( "SQL Fehler Verteilerlisten:" . mysql_error ()."\n\n".$query );
 			if (mysql_num_rows ( $checkdata ) >= 1) {
@@ -209,15 +291,14 @@ while ( $stillsending ) {
 					// echo "<p>EMail ".$row->email."</p>";
 					$email = $row->email;
 					$name = $row->firstname . " " . $row->lastname;
-					
-					sendPmAfterCheck ( $counter, $sendstate, $email, $all_mails, $pmid, $subject, $body, $email, $name, $pdf, $verbose, $distribution_id );
+					$hash = $row->hash;
+					sendPmAfterCheck ( $counter, $sendstate, $email, $all_mails, $pmid, $subject, $body, $name, $pdf, $verbose, $distribution_id, $hash );
 				}
 			}
 		}
 		If ($send_customer) {
 			// Hier kommen die Einzelempfänger
-			
-			$query = "SELECT customerID FROM pressreleaseconnection WHERE pressreleaseID = $pmid";
+			$query = "SELECT customerID FROM pressreleaseconnection WHERE pressreleaseID = $pmid AND customerID IS NOT NULL";
 			$checkdata = mysql_query ( $query )  or die ( "SQL Fehler Einzelempfänger:" . mysql_error ()."\n\n".$query );
 			if (mysql_num_rows ( $checkdata ) >= 1) {
 				// $counter = 0;
@@ -225,19 +306,17 @@ while ( $stillsending ) {
 					$counter ++;
 					// Ich hole mir zu jedem Einzelempfänger die Daten
 					If (! empty ( $row->customerID )) {
-						$query2 = "SELECT email,firstname,lastname FROM customer WHERE id = $row->customerID AND deleted!=1";
-						$checkdata2 = mysql_query ( $query2 );
+						$query2 = "SELECT email,firstname,lastname,id,hash FROM customer WHERE id = $row->customerID AND deleted!=1 AND deleted!=2";
+						$checkdata2 = mysql_query ( $query2 ) or die ( "SQL Fehler Einzelempfänger:" . mysql_error ()."\n\n".$query );
 						if (mysql_num_rows ( $checkdata2 ) == 1) {
 							while ( $row2 = mysql_fetch_object ( $checkdata2 ) ) {
 								// echo "<p>EMail ".$row2->email."</p>";
 								$email = $row2->email;
 								$name = $row2->firstname . " " . $row2->lastname;
+								$hash = $row2->hash;
 							}
-						} else {
-							$email = "no@real.mail"; // Damit keine Aktion erfolgt, falls der gesperrt wurde
-						}
-						
-						sendPmAfterCheck ( $counter, $sendstate, $email, $all_mails, $pmid, $subject, $body, $email, $name, $pdf, $row->id, $verbose, "Einzelempfänger" );
+							sendPmAfterCheck ( $counter, $sendstate, $email, $all_mails, $pmid, $subject, $body, $name, $pdf, $verbose,$row->customerID, $hash );
+						} 
 					}
 				}
 			}
@@ -245,23 +324,27 @@ while ( $stillsending ) {
 		// Mitteilung an die Presse
 		$email = 'presse@piratenfraktion-sh.de';
 		$name = 'Christian Lewin';
-		sendpm ( $subject." - KontrollPM", $body, $email, $name, $pdf, '0' );
+		sendpm ( $subject." - KontrollPM", $body, $email, $name, $pdf, '0', '0' );
 		If ($verbose) {
 			echo "PM an Presse als Kopie verschickt...\n";
 		}
-		
-
+	
 		// PM als gesendet markieren
 		setpmsending ( $pmid, - 2 );
-
-		flock ( $fp, LOCK_UN );
-		fclose ( $fp );
-		unset ( $all_mails );
 	}
+
+	unset ( $all_mails );
 }
 If ($verbose) {
 	echo "Ich bin hier fertig.\n";
 }
+
+
+
+$file = './lastSendEMail.txt';
+$current = date("d.m.Y H:i:s");
+// Schreibt den Inhalt in die Datei zurück
+file_put_contents($file, $current);
 
 ?>
 
